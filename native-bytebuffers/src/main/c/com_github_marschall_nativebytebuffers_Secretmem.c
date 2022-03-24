3c803caee4c55f2fa100831c99f3393007ebe9ef
Philippe Marschall <philippe.marschall@gmail.com>
2022-03-24 21:59:44 +0100

    switch to syscall as glibc wrapper is missing

--- native-bytebuffers/src/main/c/com_github_marschall_nativebytebuffers_Secretmem.c
+++ native-bytebuffers/src/main/c/com_github_marschall_nativebytebuffers_Secretmem.c
@@ -1,10 +1,9 @@
 
-#if __has_include(<linux/secretmem.h>)
-#define _secretmem
-#endif
+#include <sys/syscall.h>      /* Definition of SYS_* constants */
+#include <unistd.h>
 
-#ifdef _secretmem
-# include <linux/secretmem.h>
+#ifdef SYS_memfd_secret
+#define _secretmem
 #endif
 
 #include <jni.h>
@@ -12,12 +11,20 @@
 #include "jniUtil.h"
 #include "com_github_marschall_nativebytebuffers_Secretmem.h"
 
+
+#ifdef _secretmem
+static inline int memfd_secret(unsigned int flags)
+{
+  return syscall(SYS_memfd_secret, flags);
+}
+#endif
+
 JNIEXPORT jint JNICALL Java_com_github_marschall_nativebytebuffers_Secretmem_memfd_1secret0
   (JNIEnv *env, jclass clazz, jlong flags)
 {
 #ifdef _secretmem
   _Static_assert (sizeof(jint) == sizeof(unsigned int), "sizeof(jint) == sizeof(unsigned int)");
-  int fd =  memfd_secret(name, (unsigned int) flags);
+  int fd =  memfd_secret((unsigned int) flags);
   if (fd != -1)
   {
     return fd;
